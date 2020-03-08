0f71951c60d6173c6fb3cbd1f9d9b8035d8d6104
wanglinawln <1416978302@qq.com>
2020-03-08 15:09:57 +0800

    v0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,11 +59,15 @@ module.exports = function(grunt) {
             html:['dist/index.html']
         },
         concat:{
+            options:{
+                separator:';'
+            },
             js:{
                 src:['rectangle.js','calc.js'],
                 dest:'dist/bundle.js'
             }
         },
+        clean:['dist/bundle.js','.tmp'],
         copy:{
             html:{
                 src:'./index.html',
@@ -81,8 +85,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
     grunt.registerTask('unitTest', ['mocha']);
-    grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin']);
+    grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
 };
